ce3b5ba8bf6f276f1314d372b61da65535a3c167
maruf <dev.maruf112@gmail.com>
2023-07-25 11:51:14 +0600

    21-2 Cart slice and handling add product.

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        cart: cartReducer
+    },
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
